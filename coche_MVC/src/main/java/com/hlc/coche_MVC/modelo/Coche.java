070a62a43a9bc6093226fec8a75a8a2ec3ba4f0f
Miguel Ángel Luque Pérez <miguelangellupe2005@gmail.com>
2025-01-09 09:10:46 +0100

    Fallos arreglados en el Modelo

--- coche_MVC/src/main/java/com/hlc/coche_MVC/modelo/Coche.java
+++ coche_MVC/src/main/java/com/hlc/coche_MVC/modelo/Coche.java
@@ -1,49 +1,49 @@
 package com.hlc.coche_MVC.modelo;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Coche {
-	@Id
-	private int id;
-	private String marca;
-	private String matricula;
-	private String color;
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String marca;
+    private String matricula;
+    private String color;
 
-	public int getId() {
-		return id;
-	}
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getMarca() {
-		return marca;
-	}
+    public String getMarca() {
+        return marca;
+    }
 
-	public void setMarca(String marca) {
-		this.marca = marca;
-	}
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
 
-	public String getMatricula() {
-		return matricula;
-	}
+    public String getMatricula() {
+        return matricula;
+    }
 
-	public void setMatricula(String matricula) {
-		this.matricula = matricula;
-	}
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
 
-	public String getColor() {
-		return color;
-	}
+    public String getColor() {
+        return color;
+    }
 
-	public void setColor(String color) {
-		this.color = color;
-	}
-	
-	
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
